e0f13028473fa7f376ed2401862e0a7308b56e1b
Natalia Kramarz <nat.kramarz@gmail.com>
2022-12-01 08:49:02 +0100

    day 1 solution

---
+++ day1.py
@@ -0,0 +1,17 @@
+with open("calories.txt") as f:
+    calories = []
+    curr_count = 0 
+    for line in f:
+        if line == '\n':
+            calories.append(curr_count)
+            curr_count = 0
+        else:
+            curr_count += int(line.rstrip())
+    # part 1 
+    # print(max(calories))
+    first_max = max(calories)
+    calories.remove(first_max)
+    second_max = max(calories)
+    calories.remove(second_max)
+    third_max = max(calories)
+    print(first_max + second_max + third_max)
