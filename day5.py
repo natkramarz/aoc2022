4cc6aaec2a534e29a6796ba79e8f2472b70f4843
Natalia Kramarz <nat.kramarz@gmail.com>
2022-12-05 12:41:39 +0100

    add day 5

---
+++ day5.py
@@ -0,0 +1,50 @@
+import re
+from collections import deque
+
+# part 1
+with open("tests/day5.txt") as f:
+    stacks = []
+    for line in f:
+        processed_line = line.rstrip()
+        if re.match("^\s*\[", processed_line):
+            for i in range(0, len(processed_line), 4):
+                stack_index = i // 4
+                if len(stacks) <= stack_index:
+                    stacks.append(deque())
+                if processed_line[i:i+3] != "   ":
+                    stacks[stack_index].appendleft(processed_line[i+1])
+        else:
+            matched = re.match("^move (\d+) from (\d+) to (\d+)$", processed_line)
+            if matched is not None:
+                from_index = int(matched.groups()[1]) - 1
+                to_index = int(matched.groups()[2]) - 1
+                for _ in range(int(matched.groups()[0])):
+                    crate = stacks[from_index].pop()
+                    stacks[to_index].append(crate)
+    print("".join([stack[-1] for stack in stacks]))
+
+# part 2 
+with open("tests/day5.txt") as f:
+    stacks = []
+    for line in f:
+        processed_line = line.rstrip()
+        if re.match("^\s*\[", processed_line):
+            for i in range(0, len(processed_line), 4):
+                stack_index = i // 4
+                if len(stacks) <= stack_index:
+                    stacks.append(deque())
+                if processed_line[i:i+3] != "   ":
+                    stacks[stack_index].appendleft(processed_line[i+1])
+        else:
+            matched = re.match("^move (\d+) from (\d+) to (\d+)$", processed_line)
+            if matched is not None:
+                from_index = int(matched.groups()[1]) - 1
+                to_index = int(matched.groups()[2]) - 1
+                curr_crates = []
+                for _ in range(0, int(matched.groups()[0])):
+                    crate = stacks[from_index].pop()
+                    curr_crates.append(crate)
+                for crate in reversed(curr_crates):
+                    stacks[to_index].append(crate)
+    print(stacks)
+    print("".join([stack[-1] for stack in stacks if len(stack) != 0]))
